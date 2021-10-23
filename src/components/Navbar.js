f7cf64e6ae019ac26898cfb382243cf16de16327
Nirajan Mahara <mnirajan9@gmail.com>
2021-10-23 14:09:44 +0545

    Add `{ logo }` from `data.json`

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import '../styles/Navbar.css';
 import { Link } from 'react-router-dom';
+import { logo } from '../data/data.json';
 
 const Navbar = () => {
   return (
     <div className='nav'>
       <div className='logo'>
         <Link to='/'>
-          <img id='logoImage' src='' alt='Logo' />
+          <img id='logoImage' src={logo} alt='Logo' />
         </Link>
       </div>
     </div>
