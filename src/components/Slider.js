50eda1e060f3f7577e9cf55c2b90ec3ca42c1ce4
Nirajan Mahara <mnirajan9@gmail.com>
2021-10-23 15:51:59 +0545

    Hide `<Label>` from `Carousel`

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -4,7 +4,7 @@ import Carousel from 'react-bootstrap/Carousel';
 
 const Slider = ({ start }) => {
   return (
-    <Carousel fade>
+    <Carousel fade nextLabel={null} prevLabel={null}>
       {start.map((item) => (
         <Carousel.Item>
           <img
