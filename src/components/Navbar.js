da900dbdb55a1b0801718e37d7ed1811361e08f9
Nirajan Mahara <mnirajan9@gmail.com>
2021-10-23 14:32:37 +0545

    Add `styles` to `.navLinks` in `Navbar` component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,6 +11,15 @@ const Navbar = () => {
           <img id='logoImage' src={logo} alt='Logo' />
         </Link>
       </div>
+
+      <Link className="navLinks" to="/#miphones">Mi Phones</Link>
+      <Link className="navLinks" to="/#redmiphones">Redmi Phones</Link>
+      <Link className="navLinks" to="/#tv">TV</Link>
+      <Link className="navLinks" to="/#laptops">Laptops</Link>
+      <Link className="navLinks" to="/#lifestyle">Fitness & Lifestyle</Link>
+      <Link className="navLinks" to="/#home">Home</Link>
+      <Link className="navLinks" to="/#audio">Radio</Link>
+      <Link className="navLinks" to="/#accessories">Accessories</Link>
     </div>
   );
 };
