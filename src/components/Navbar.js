596acc2b25c79b8edc7f40e505efd6e98b1ac419
Nirajan Mahara <mnirajan9@gmail.com>
2021-10-23 14:07:25 +0545

    Add `{ }` from `npm i react-router-dom`

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import '../styles/Navbar.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div className='nav'>
-      Navbar
+      <div className='logo'>
+        <Link to='/'>
+          <img id='logoImage' src='' alt='Logo' />
+        </Link>
+      </div>
     </div>
   );
 };
