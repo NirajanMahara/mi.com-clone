927f801636f99e7c6d0876cdf74da112e8f26bf9
Nirajan Mahara <mnirajan9@gmail.com>
2021-10-23 15:16:44 +0545

    Add `Carousel` from `npm i react-bootstrap bootstrap@4.6.0` [temp]

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import PreNavbar from './components/PreNavbar';
 import Navbar from './components/Navbar.js';
 import { BrowserRouter as Router } from 'react-router-dom';
+import Slider from './components/Slider.js';
 
 function App() {
   return (
     <Router>
       <PreNavbar />
       <Navbar />
+      <Slider />
     </Router>
   );
 }
