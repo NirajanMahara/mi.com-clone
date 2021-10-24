2eaf2f0b19ac431109ea4dfc13cf907f1c532ea7
Nirajan Mahara <mnirajan9@gmail.com>
2021-10-24 21:36:32 +0545

    Add `Browse More` last `HotItemCard` in `HotAccessories` Component `[final]`

--- src/components/HotAccessories.js
+++ src/components/HotAccessories.js
@@ -60,6 +60,8 @@ const HotAccessories = ({ music, musicCover, smartDevice, smartDeviceCover, home
             index={index}
           />
         ))}
+        
+        <HotItemCard image="https://i01.appmifile.com/webfile/globalimg/in/cms/593181BE-7DB8-15F5-E907-506427B8C9F4.jpg" />
       </div>
     </div>
   );
