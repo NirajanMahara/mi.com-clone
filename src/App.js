27147b4d589316513a946591ae67d49c6c71f3ca
Nirajan Mahara <mnirajan9@gmail.com>
2021-10-25 08:00:53 +0545

    Add dynamic `Carousel img` in `Banner` component by passing `data from data.json` using `array.end.map()`

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import HotAccessoriesMenu from './components/HotAccessoriesMenu.js';
 import HotAccessories from './components/HotAccessories.js';
 import ProductReviews from './components/ProductReviews.js';
 import Videos from './components/Videos.js';
+import Banner from './components/Banner.js';
 
 function App() {
   return (
@@ -64,6 +65,7 @@ function App() {
       <Heading text='VIDEOS' />
       <Videos videos={data.videos} />
       <Heading text='IN THE PRESS' />
+      <Banner banner={data.banner} />
     </Router>
   );
 }
