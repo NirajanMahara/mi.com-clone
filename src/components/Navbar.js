8cab32f71dbc6af51504d29dacfa725d0a47bdce
Nirajan Mahara <mnirajan9@gmail.com>
2021-10-25 09:55:00 +0545

    Replace for `Reload Page` replace `<Link to><Link>` to `<a href></a>`

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../styles/Navbar.css';
-import { Link } from 'react-router-dom';
 import { logo } from '../data/data.json';
 
 const searchIcon = <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></svg>
@@ -9,19 +8,19 @@ const Navbar = () => {
   return (
     <div className='nav'>
       <div className='logo'>
-        <Link to='/'>
+        <a href='/'>
           <img id='logoImage' src={logo} alt='Logo' />
-        </Link>
+        </a>
       </div>
 
-      <Link className="navLinks" to="/miphones">Mi Phones</Link>
-      <Link className="navLinks" to="/redmiphones">Redmi Phones</Link>
-      <Link className="navLinks" to="/tv">TV</Link>
-      <Link className="navLinks" to="/laptops">Laptops</Link>
-      <Link className="navLinks" to="/lifestyle">Fitness & Lifestyle</Link>
-      <Link className="navLinks" to="/home">Home</Link>
-      <Link className="navLinks" to="/audio">Radio</Link>
-      <Link className="navLinks" to="/accessories">Accessories</Link>
+      <a className="navLinks" href="/miphones">Mi Phones</a>
+      <a className="navLinks" href="/redmiphones">Redmi Phones</a>
+      <a className="navLinks" href="/tv">TV</a>
+      <a className="navLinks" href="/laptops">Laptops</a>
+      <a className="navLinks" href="/lifestyle">Fitness & Lifestyle</a>
+      <a className="navLinks" href="/home">Home</a>
+      <a className="navLinks" href="/audio">Radio</a>
+      <a className="navLinks" href="/accessories">Accessories</a>
 
       <div className='searchBox'>
         <input type='text' name='search' placeholder='Search Products' />
